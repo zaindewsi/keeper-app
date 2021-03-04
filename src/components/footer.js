21ef5f4ab44961d36841c5ac9bbbb09efaf2a274
Zain Dewsi <zaindewsi@Zains-MacBook-Pro.local>
2021-03-04 13:16:05 -0500

    First commit

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Footer() {
+    const year = new Date().getFullYear();
+
+    return <footer><p>Copyright &#169; {year}</p></footer>
+}
+
+export default Footer;
